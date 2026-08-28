0fdfbbfe59a5f39d6630e17674338f429c8da7f6
Krovely <rosakryg@gmail.com>
2026-08-28 16:01:10 +0300

    Add files via upload

---
+++ lesson_2_0.py
@@ -0,0 +1,10 @@
+elements = [12,3,4,10]
+elements = [1]
+elements = []
+elements = [12, 3, 4, 10, 8]
+elements.append(1)
+elements.insert(0,elements.pop(len(elements)-2))
+elements.pop()
+print(elements)
+
+
